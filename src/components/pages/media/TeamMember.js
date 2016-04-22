c2254a5b56d668df2874ec9b04bd4f9eb5baecaa
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-04-22 15:59:12 +0000

    now with teamPage

---
+++ src/components/pages/media/TeamMember.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const TeamMember = ({ name, role, type, description, src }) => {
+  return (
+    <div className='columns'>
+      <div className='column is-3'>
+        <div className='media-content'>
+          <figure className='content'>
+            <img className='center-img' src={src}/>
+          </figure>
+        </div>
+      </div>
+      <div className='column is-9'>
+        <div className='container'>
+          <p><strong>{name}. {role}</strong> {description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+TeamMember.propTypes = {
+  src: React.PropTypes.string.isRequired,
+  name: React.PropTypes.string.isRequired,
+  description: React.PropTypes.string.isRequired,
+  type: React.PropTypes.string.isRequired,
+  role: React.PropTypes.string
+}
+
+export default TeamMember
