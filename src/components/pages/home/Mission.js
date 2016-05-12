dc3c1e6d89642e9191f3e4cad04bb25a4ef026e9
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-05-11 19:43:05 -0700

    update nav and include smooth-scroll

--- src/components/pages/home/Mission.js
+++ src/components/pages/home/Mission.js
@@ -14,17 +14,13 @@ const Mission = React.createClass({
           <br/>
           <p>
           Mind Makers Incorporation is a 501(c)(3) nonprofit organization working to empower adults from unconventional backgrounds with engineering skills and mentor support and provide them with pathways to make a positive impact on their community in return. The model is a propagating outreach initiative that diversifies the Science, Technology, Engineering, Arts and Math (STEAM) world by educating an unexpected group of adults and activating them to pass it on to other adults and children.
-          <br /><br />
-          
+            <br /><br />
           The program engages engineers and educators who share their knowledge, free of charge, designing and teaching an approachable yet intense workshop-based, ìmakerî oriented curriculum. The Mind Makers educational structure is designed in a supportive, boot camp style which has gained popularity with the public for software education. However, in addition to teaching software, some of these new skills include elements of robotics, computer programing, CAD, electrical and mechanical engineering. Once completed, all of the educational content and individual experiences will be provided as open-source resources.
-          <br /><br />
-          
+            <br /><br />
           Through collaboration with professional engineers and designers, student workshops culminate in the creation of an interactive exhibit which uses automation to recreate the human senses. This serves the purpose of revealing the magic of both robotics and anatomy to a larger community, but also demonstrates two beautiful concepts. One is that magic can be made by combining STEM knowledge with mentorship. The other, is that truly anyone can take part in this magic and then make more by passing it on to others.
-          <br /><br />
-
+            <br /><br />
           This message is delivered beyond the exhibit by showcasing the stereotype-breaking team who creates it and then sending them as Mind Makers alumni into the community and classrooms of children to share their new knowledge and serve as diverse examples of technically skilled role models.
-          <br /><br />
-
+            <br /><br />
           The immediate audience for Mind Makers is working-age adults, who are overlooked by many engineering education outreach initiatives in lieu of working directly with children. By helping adults to attain confidence, technical experience and a passion for knowledge sharing, Mind Makers serves adults to the community as agents of change and examples redefining possibility for people of all ages. Together, we can empower all generations with the gifts of knowledge and show that impactful people are everywhere around us.
           </p>
         </div>
