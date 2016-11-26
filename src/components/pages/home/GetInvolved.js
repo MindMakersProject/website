26c23529331a6477880672cd2720e2c7c260b8ef
wcjohnson11 <williamcouperjohnson@gmail.com>
2016-11-26 13:47:07 -0800

    rebase develop branch

--- src/components/pages/home/GetInvolved.js
+++ src/components/pages/home/GetInvolved.js
@@ -1,36 +1,55 @@
 import React from 'react'
-import JobPosting from './../media/contact/JobPosting.js'
-import jobs from './../media/contact/jobs.js'
+import JobPosting from './../media/get-involved/JobPosting.js'
+import jobs from './../media/get-involved/jobs.js'
 
 const GetInvolved = React.createClass({
   getInitialState () {
     return {
-      jobPostings: []
+      workshopJobs: [],
+      curriculumJobs: []
     }
   },
   componentDidMount () {
-    const jobPostings = jobs.map(function (job, index) {
-      return (
+    let workshopJobs = [];
+    let curriculumJobs = [];
+    jobs.map((job, index) => {
+      let posting = 
         <JobPosting
           title={job.title}
           description={job.description}
-          address={job.address}
+          address='volunteer@mindmakersproject.org'
           key={index}
         />
-      )
+
+      if (job.type === 'Curriculum') {
+        curriculumJobs.push(posting);
+      } else {
+        workshopJobs.push(posting);
+      }
     })
-    this.setState({jobPostings: jobPostings})
+
+    this.setState({
+      workshopJobs: workshopJobs,
+      curriculumJobs: curriculumJobs
+    })
   },
   render () {
     return (
       <section id='get-involved' className='section'>
-        <div className='container heading is-text-centered'>
+        <div className='container is-text-centered'>
           <h1 className='title'>Get Involved</h1>
-          {this.state.jobPostings}
+          <h2 className='subtitle'><strong>We're building a one-of-a-kind, open-source curriculum with virtual content and collaborative hardware building modules. Our team is diverse, building content that ties together neuroscience, robotics, anatomy, data visualization, system networking and philosophy. Join our team!</strong></h2>
+          <h2 className='subtitle is-4'>Curriculum Development</h2>
+          <p>Great for creatives & technical types worldwide.</p>
+          <br/>
+          {this.state.curriculumJobs}
+          <h2 className='subtitle is-4'>Workshop Team</h2>
+          <p>Makers & movers in the LA area and abroad.</p>
+          <br/>
+          {this.state.workshopJobs}
         </div>
       </section>
     )
   }
 })
-
 export default GetInvolved
